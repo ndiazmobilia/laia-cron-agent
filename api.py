a7b2771a9d8884a5cac8c5def911d695b6036ea8
Clinica Lubens <clinicalubens@gmail.com>
2025-08-03 23:53:12 +0200

    refactor: use flask logger instead of print

--- api.py
+++ api.py
@@ -1,7 +1,13 @@
 from flask import Flask, request, jsonify
 import message_handler
+import logging
 
 app = Flask(__name__)
+
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
 
 @app.route("/process_message", methods=["POST"])
 def process_message():
@@ -9,11 +15,12 @@ def process_message():
     message = data.get("message")
     chat_id = data.get("chat_id")
     source = data.get("source")
-    print(f"[api.py] Received message for processing: message='{message}', chat_id='{chat_id}', source='{source}'")
+    app.logger.info(f"Received message for processing: message='{message}', chat_id='{chat_id}', source='{source}'")
     response = message_handler.handle_message(message, chat_id, source)
-    print(f"[api.py] Sending response: {response}")
+    app.logger.info(f"Sending response: {response}")
     return jsonify({"response": response})
 
 if __name__ == "__main__":
-    print("[api.py] Starting Flask API on port 5000...")
+    app.logger.setLevel(logging.INFO)
+    app.logger.info("Starting Flask API on port 5000...")
     app.run(port=5000)
